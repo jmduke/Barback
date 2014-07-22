ebcf8de72ecd356ee505a019cce7673da8b0e23f
Justin Duke <me@jmduke.com>
2014-07-21 22:41:56 -0700

    Conform to beta4.

--- BarbackSwift/RecipeListViewController.swift
+++ BarbackSwift/RecipeListViewController.swift
@@ -70,7 +70,7 @@ class RecipeListViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView!, heightForRowAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
-        return UITableViewCell().primaryCellHeight()
+        return CGFloat(UITableViewCell().primaryCellHeight())
     }
     
     func getSelectedRecipe() -> Recipe {
