787d65bcc66cc0d361dbb8e0bf9057cab53beab2
Justin Duke <me@jmduke.com>
2014-10-20 22:17:02 -0700

    Move brands to separate json and backload

--- BarbackSwift/Brand.swift
+++ BarbackSwift/Brand.swift
@@ -25,21 +25,34 @@ class Brand: BarbackModel {
         }
     }
     
-    class func fromAttributes(name: String, price: Int) -> Brand {
+    class func fromAttributes(name: String, price: Int, base: IngredientBase) -> Brand {
         let newBrand: Brand = NSEntityDescription.insertNewObjectForEntityForName("Brand", inManagedObjectContext: managedContext()) as Brand
         newBrand.name = name
         newBrand.price = price
+        newBrand.ingredient = base
         return newBrand
     }
     
     class func fromDict(rawBrand: NSDictionary) -> Brand {
         let name = rawBrand.objectForKey("name") as String
         let price = rawBrand.objectForKey("price") as Int
-        return fromAttributes(name, price: price)
+        let baseName = rawBrand.objectForKey("base") as String
+        
+        let base = IngredientBase.forName(baseName) as IngredientBase
+        
+        return fromAttributes(name, price: price, base: base)
     }
     
     override class func entityName() -> String {
         return "Brand"
     }
     
+    class func fromJSONFile(filename: String) -> [Brand] {
+        let filepath = NSBundle.mainBundle().pathForResource(filename, ofType: "json")
+        let jsonData = NSString.stringWithContentsOfFile(filepath!, encoding:NSUTF8StringEncoding, error: nil)
+        let ingredientData = jsonData.dataUsingEncoding(NSUTF8StringEncoding)
+        var rawBrands = NSJSONSerialization.JSONObjectWithData(ingredientData!, options: nil, error: nil) as [NSDictionary]
+        
+        return rawBrands.map({Brand.fromDict($0)})
+    }
 }
