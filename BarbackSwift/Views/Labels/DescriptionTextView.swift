882ab6deb8467b839fa13003a88224057ea51222
Justin Duke <me@jmduke.com>
2014-11-28 14:18:27 -0500

    [refactor] [#112] Change desRecipe.description view to a UITextView so links work

--- BarbackSwift/Views/Labels/DescriptionTextView.swift
+++ BarbackSwift/Views/Labels/DescriptionTextView.swift
@@ -1,15 +1,15 @@
 //
-//  DescriptionLabel.swift
+//  DescriptionTextView.swift
 //  Barback
 //
-//  Created by Justin Duke on 11/18/14.
+//  Created by Justin Duke on 11/28/14.
 //  Copyright (c) 2014 Justin Duke. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class DescriptionLabel : UILabel {
+class DescriptionTextView : UITextView {
     
     var markdownText: NSString? {
         didSet {
@@ -19,15 +19,15 @@ class DescriptionLabel : UILabel {
             }
             
             var markdownEngine = Markdown()
-            let hexColor = NSString(format:"%2X", Color.Light.rawValue)
+            let normalColor = NSString(format:"%2X", Color.Light.rawValue)
+            let tintColor = NSString(format:"%2X", Color.Tint.rawValue)
             var informationHTML = "<style type='text/css'>"
                 + "p { text-align: center;"
                 + "    font-family: \(font.familyName);"
                 + "    font-size: \(font.pointSize)px;"
-                + "    color: \(hexColor); }"
+                + "    color: \(normalColor); }"
                 + "</style>"
             informationHTML += markdownEngine.transform(markdownText!)
-            print(informationHTML)
             let informationData = informationHTML.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
             let informationString = NSAttributedString(data: informationData!, options: [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType], documentAttributes: nil, error: nil)
             attributedText = informationString
@@ -58,5 +58,7 @@ class DescriptionLabel : UILabel {
         font = UIFont(name: UIFont.heavyFont(), size: 15)
         textAlignment = NSTextAlignment.Center
         textColor = Color.Light.toUIColor()
+        backgroundColor = Color.Background.toUIColor()
+        tintColor = Color.Tint.toUIColor()
     }
 }
