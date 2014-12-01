2d28623945eda354b1cfd0e2a76cf6f0bba645f7
Justin Duke <me@jmduke.com>
2014-11-30 22:37:31 -0800

    [refactor] Synx round two.

---
+++ Barback/Views/Labels/DescriptionLabel.swift
@@ -0,0 +1,38 @@
+//
+//  DescriptionLabel.swift
+//  Barback
+//
+//  Created by Justin Duke on 11/18/14.
+//  Copyright (c) 2014 Justin Duke. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DescriptionLabel : UILabel {
+    
+    override init() {
+        super.init()
+        styleLabel()
+    }
+    
+    // It is so stupid that I need the below constructors.
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        styleLabel()
+    }
+    
+    func styleLabel() {
+        font = UIFont(name: UIFont.heavyFont(), size: 15)
+        textAlignment = NSTextAlignment.Center
+        textColor = Color.Light.toUIColor()
+    }
+}
