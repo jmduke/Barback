c9ce69f3f204196afbba889a6b946583ca5572c3
Justin Duke <me@jmduke.com>
2014-12-07 20:33:21 -0800

    [refactor] Split utilities into sensical files.

--- Barback/Utilities/CommonUtilities.swift
+++ Barback/Utilities/CommonUtilities.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+
+func runningOnIPad() -> Bool {
+    return UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad
+}
+
+func getRecipeDetailController(segue: UIStoryboardSegue?) -> RecipeDetailViewController {
+    if runningOnIPad() {
+        return segue!.destinationViewController.topViewController as RecipeDetailViewController
+    } else {
+        return segue!.destinationViewController as RecipeDetailViewController
+    }
+}
+
+let externalUrl = NSURL(scheme: "http", host: "getbarback.com", path: "/")
