9e502f8e8ac12fc9b65f3139145cfdfd5e00ac14
Justin Duke <me@jmduke.com>
2014-12-20 19:25:23 -0800

    [etc] Update Pods

--- Pods/Headers/Public/MagicalRecord/MagicalRecord/NSAttributeDescription+MagicalDataImport.h
+++ Pods/Headers/Public/MagicalRecord/MagicalRecord/NSAttributeDescription+MagicalDataImport.h
@@ -1,1 +1,16 @@
-../../../../MagicalRecord/MagicalRecord/Categories/DataImport/NSAttributeDescription+MagicalDataImport.h
+//
+//  NSAttributeDescription+MagicalDataImport.h
+//  Magical Record
+//
+//  Created by Saul Mora on 9/4/11.
+//  Copyright 2011 Magical Panda Software LLC. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSAttributeDescription (MagicalRecord_DataImport)
+
+- (NSString *) MR_primaryKey;
+- (id) MR_valueForKeyPath:(NSString *)keyPath fromObjectData:(id)objectData;
+
+@end
