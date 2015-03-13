3d0ffa2ee6e19f4f72eb7da79fc23815db2e9da5
Justin Duke <jmduke@Justins-MacBook-Pro.local>
2015-03-12 21:08:42 -0700

    i discovered underscore and now everything is prettier.

--- Frontend/cloud/main.js
+++ Frontend/cloud/main.js
@@ -1,4 +1,23 @@
 require('cloud/app.js');
+var _ = require('underscore');
+
+// Returns list of tuples (hex, amount)
+function visualDataForRecipe(ingredients, bases) {
+  baseNames = _.map(bases, function(base) {
+    return base.attributes.name;
+  });
+  return _.sortBy(_.map(ingredients, function(ingredient) {
+    baseIndex = baseNames.indexOf(ingredient.attributes.base);
+    if (baseIndex < 0) {
+      color = "ccc";
+    } else {
+      color = bases[baseIndex].attributes.color;
+    }
+    return [color, ingredient.attributes.amount];
+  }), function(visualTuple) {
+    return visualTuple[1]
+  });
+}
 
 function objectsForVariable(className, attribute, value, callback) {
   var query = new Parse.Query(className);
@@ -54,16 +73,13 @@ Parse.Cloud.define("searchForTerm", function(request, response) {
 });
 
 Parse.Cloud.define("allRecipes", function(request, response) {
-  var results = {'name': "", 'visualData': {}};
+  var results = {};
 
   retrieved_ingredients = [];
   retrieved_recipes = [];
   var query = new Parse.Query("Recipe");
   query.limit(1000);
   query.find().then(function(recipes) {
-    for (i = 0; i < recipes.length; i++) {
-      results["visualData"][recipes[i].attributes.name] = [];
-    }
     retrieved_recipes = recipes;
     query = new Parse.Query("Ingredient");
     query.limit(1000);
@@ -73,24 +89,17 @@ Parse.Cloud.define("allRecipes", function(request, response) {
     query = new Parse.Query("IngredientBase");
     return query.find();
   }).then(function(bases) {
-    colorsForBases = {}
-    for(i = 0; i < bases.length; i++) {
-      base = bases[i];
-      colorsForBases[base.attributes.name] = base.attributes.color;
-    }
-    for(i = 0; i < retrieved_ingredients.length; i++) {
-      ingredient = retrieved_ingredients[i];
-      amount = ingredient.attributes.amount;
-      color = colorsForBases[ingredient.attributes.base];
-      results["visualData"][ingredient.attributes.recipe].push([color == null ? "ccc" : color, amount == null ? 0 : amount]);
-    }
-    results["recipes"] = [];
-    for(i = 0; i < retrieved_recipes.length; i++) {
-      recipe = retrieved_recipes[i];
-      if (results["visualData"][recipe.attributes.name].length > 1) {
-        results["recipes"].push(recipe);
-      }
-    }
+    results["visualData"] = _.object(_.map(retrieved_recipes, function(recipe) {
+      recipeIngredients = _.filter(retrieved_ingredients, function(ingredient) {
+        return ingredient.attributes.recipe == recipe.attributes.name
+      });
+      return [recipe.attributes.name, visualDataForRecipe(recipeIngredients, bases)];
+    }));
+    results["recipes"] = _.sortBy(_.filter(retrieved_recipes, function(recipe) {
+      return results["visualData"][recipe.attributes.name].length > 1
+    }), function(recipe) {
+      return recipe.attributes.name
+    });
     response.success(results);
   });
 });
@@ -100,39 +109,26 @@ Parse.Cloud.define("recipeForName", function(request, response) {
   objectsForVariable("Recipe", "name", request.params.name, function(recipes) {
     recipe = recipes[0];
     objectsForVariable("Ingredient", "recipe", request.params.name, function(ingredients) {
-      recipes_left = ingredients.length;
       recipe.attributes.ingredients = ingredients;
-      baseNames = [];
-      for (i = 0; i < ingredients.length; i++) {
-        baseNames.push(ingredients[i].attributes.base);
-      }
+      baseNames = _.map(ingredients, function(ingredient) {
+        return ingredient.attributes.base;
+      });
       objectsForVariables("IngredientBase", "name", baseNames, function(bases) {
         recipe.attributes.bases = [];
         recipe.attributes.visualData = []; 
-        nominator = 0.0;
-        denominator = 0.0;
-        for (j = 0; j < bases.length; j++) {
-          base = bases[j];
+        recipe.attributes.nominator = 0.0;
+        recipe.attributes.denominator = 0.0;
+        _.each(bases, function(base) {
           recipe.attributes.bases.push(base.attributes.name);
           abv = base.attributes.abv;
           amount = ingredients[baseNames.indexOf(base.attributes.name)].attributes.amount;
-          recipe.attributes.visualData.push([base.attributes.color, amount == null ? 0 : amount]);
-          if ((abv > 0) && (amount > 0)) {
-            nominator += abv * amount;
-            denominator += amount;
-          } else if (amount > 0) {
-            denominator += amount;
-          }
-        }
-        for (i = 0; i < ingredients.length; i++) {
-          ingredient = ingredients[i];
-          if (recipe.attributes.bases.indexOf(ingredient.attributes.base) < 0) {
-            recipe.attributes.visualData.push(["ccc", ingredient.attributes.amount]);
-          }
-        }
-        recipe.attributes.nominator = nominator;
-        recipe.attributes.denominator = denominator;
-        recipe.attributes.abv = Math.round(nominator / denominator);
+          amount = amount == null ? 0 : amount;
+          recipe.attributes.nominator += abv * amount;
+          recipe.attributes.denominator += amount;
+        });
+
+        recipe.attributes.visualData = visualDataForRecipe(ingredients, bases);
+        recipe.attributes.abv = Math.round(recipe.attributes.nominator / recipe.attributes.denominator);
         response.success(recipe);
       });
     });
