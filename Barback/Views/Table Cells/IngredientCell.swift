1b7b0fb6eec73b7096f5ff400b8ebcb74e3c93f6
Justin Duke <jmduke@Justins-MacBook-Pro.local>
2015-04-04 16:15:07 -0700

    more styling with colors.

--- Barback/Views/Table Cells/IngredientCell.swift
+++ Barback/Views/Table Cells/IngredientCell.swift
@@ -1,9 +1,46 @@
 //
-//  IngredientCell.swift
+//  RecipeCell.swift
 //  Barback
 //
-//  Created by Justin Duke on 4/4/15.
-//  Copyright (c) 2015 Justin Duke. All rights reserved.
+//  Created by Justin Duke on 11/17/14.
+//  Copyright (c) 2014 Justin Duke. All rights reserved.
 //
 
 import Foundation
+
+class IngredientCell : StyledCell {
+    
+    var ingredient: Ingredient?
+
+    init(ingredient: Ingredient, reuseIdentifier: String?) {
+        super.init(style: UITableViewCellStyle.Subtitle,
+            reuseIdentifier: reuseIdentifier)
+        
+        textLabel?.text = ingredient.base.name
+        detailTextLabel?.text = ingredient.label
+        
+        let diagram = UIBezierPath(ovalInRect: CGRect(x: 0.0, y: 0.0, width: 30.0, height: 30.0))
+        if let color = ingredient.base.color {
+            let fillColor = UIColor.fromHex(color)
+            let image = diagram.toImageWithStrokeColor(fillColor, fillColor: fillColor)
+            let imageView = UIImageView(image: image)
+            imageView.frame = CGRect(x: 15.0, y: 15.0, width: 30.0, height: 30.0)
+            addSubview(imageView)
+        }
+        self.ingredient = ingredient
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel?.frame.origin.x = 60
+        detailTextLabel?.frame.origin.x = 60
+        
+        if let color = ingredient!.base.color {
+        }
+    }
+    
+}
--- Barback/Views/Table Cells/RecipeCell.swift
+++ Barback/Views/Table Cells/RecipeCell.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class RecipeCell : StyledCell {
     
-    let newRecipeIconDiameter: CGFloat = 8
+    var diagram: RecipeDiagramView?
     
     init(recipe: Recipe, reuseIdentifier: String?) {
         super.init(style: UITableViewCellStyle.Subtitle,
@@ -19,24 +19,19 @@ class RecipeCell : StyledCell {
         textLabel?.text = recipe.name
         detailTextLabel?.text = recipe.detailDescription
         
-        var path: UIBezierPath?
-        if recipe.favorite {
-            let transform = CGAffineTransformMakeScale(0.8, 0.8)
-            path = UIBezierPath.favoriteButton()
-            path!.applyTransform(transform)
-        } else if (recipe.isNew) {
-            path = UIBezierPath(ovalInRect: CGRectMake(0, 0, CGFloat(newRecipeIconDiameter), CGFloat(newRecipeIconDiameter)))
-        }
-        imageView?.image = path?.toImageWithStrokeColor(Color.Lighter.toUIColor(), fillColor: Color.Lighter.toUIColor())
-        
         let diagram = RecipeDiagramView(recipe: recipe)
         diagram.strokeWidth = 1
         diagram.diagramScale = 0.4
         diagram.bgColor = UIColor.whiteColor()
+        if recipe.favorite {
+            diagram.outlineColor = Color.Tint.toUIColor()
+            diagram.outlineWidth = 4
+        }
         diagram.heightOffset = (60.0 - diagram.glassware.dimensions().0 * diagram.diagramScale) / 2.0
         diagram.widthOffset = (40.0 - diagram.glassware.dimensions().2 * diagram.diagramScale) / 2.0
         diagram.frame = CGRect(x: 10.0, y: 0.0, width: 40.0, height: 60.0)
         contentView.addSubview(diagram)
+        self.diagram = diagram
     }
     
     required init(coder: NSCoder) {
@@ -50,9 +45,6 @@ class RecipeCell : StyledCell {
         let rightPadding: CGFloat = 16
         
         let bounds = contentView.bounds
-        var frame = imageView?.frame
-        frame?.origin.x = bounds.size.width - frame!.size.width - (frame!.size.width < newRecipeIconDiameter * 2 ? rightPadding + 3 : rightPadding)
-        imageView?.frame = frame!
         
         textLabel?.frame.origin.x = leftPadding
         detailTextLabel?.frame.origin.x = leftPadding
