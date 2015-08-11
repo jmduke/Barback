e19ebc97075540b8b646b8e64e0b4c071d2b90b7
Justin Duke <dukerson@gmail.com>
2015-08-11 12:14:34 -0400

    Minor changes.

--- Barback/Controllers/HasCoachMarks.swift
+++ Barback/Controllers/HasCoachMarks.swift
@@ -1,9 +1,54 @@
-//
-//  File.swift
-//  Barback
-//
-//  Created by Justin Duke on 8/9/15.
-//  Copyright © 2015 Justin Duke. All rights reserved.
-//
-
 import Foundation
+import WSCoachMarksView
+
+protocol HasCoachMarks {
+    
+    func coachMarksForController() -> [CoachMark]
+    func runCoachMarks()
+    
+}
+
+extension HasCoachMarks {
+    
+    func runCoachMarks() {
+        if let controller = self as? UIViewController {
+            controller.runCoachMarks(coachMarksForController())
+        }
+    }
+}
+
+extension UIViewController {
+    
+    func runCoachMarks(coachMarks: [CoachMark]) {
+        
+        if coachMarks.isEmpty {
+            return
+        }
+        
+        let caption = coachMarks[0].caption
+        let prefix: AnyObject = caption.componentsSeparatedByString(" ")[0]
+        let userDefaultsKey = "coachMarksFor\(prefix)"
+        let haveCoachMarksBeenShown = NSUserDefaults.standardUserDefaults().boolForKey(userDefaultsKey) && false
+        
+        if (!haveCoachMarksBeenShown) {
+            let parsedCoachMarks: [Dictionary] = coachMarks.map {
+                ["rect": NSValue(CGRect: $0.rect), "caption": $0.caption]
+            }
+            if let controller = navigationController {
+                let coachMarksView = WSCoachMarksView(frame: controller.view.bounds, coachMarks: parsedCoachMarks)
+                coachMarksView.lblCaption.font = UIFont(name: UIFont.primaryFont(), size: 20)
+                controller.view.addSubview(coachMarksView)
+                coachMarksView.start()
+            } else {
+                let coachMarksView = WSCoachMarksView(frame: view.bounds, coachMarks: parsedCoachMarks)
+                coachMarksView.lblCaption.font = UIFont(name: UIFont.primaryFont(), size: 20)
+                view.addSubview(coachMarksView)
+                coachMarksView.start()
+            }
+            
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: userDefaultsKey)
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
+
+}
--- Barback/Extensions/UIViewControllerExtension.swift
+++ Barback/Extensions/UIViewControllerExtension.swift
@@ -8,30 +8,8 @@
 
 import Foundation
 import UIKit
-import WSCoachMarksView
 
 extension UIViewController {
-
-    func runCoachMarks(coachMarks: [CoachMark]) {
-
-        let caption = coachMarks[0].caption
-        let prefix: AnyObject = caption.componentsSeparatedByString(" ")[0]
-        let userDefaultsKey = "coachMarksFor\(prefix)"
-        let haveCoachMarksBeenShown = NSUserDefaults.standardUserDefaults().boolForKey(userDefaultsKey)
-
-        if (!haveCoachMarksBeenShown) {
-            let parsedCoachMarks: [Dictionary] = coachMarks.map {
-                ["rect": NSValue(CGRect: $0.rect), "caption": $0.caption]
-            }
-            let coachMarksView = WSCoachMarksView(frame: view.bounds, coachMarks: parsedCoachMarks)
-            coachMarksView.lblCaption.font = UIFont(name: UIFont.primaryFont(), size: 20)
-            view.addSubview(coachMarksView)
-            coachMarksView.start()
-            
-            NSUserDefaults.standardUserDefaults().setBool(true, forKey: userDefaultsKey)
-            NSUserDefaults.standardUserDefaults().synchronize()
-        }
-    }
 
     func styleController() {
         navigationController?.styleController()
