1479dbe9b807777c08b9c4e61a17b7ef26bba8bd
Justin Duke <dukerson@gmail.com>
2015-08-13 16:51:21 -0700

    More tests.

--- BarbackTests/SearchRecipeListViewControllerSpec.swift
+++ BarbackTests/SearchRecipeListViewControllerSpec.swift
@@ -43,6 +43,61 @@ class SearchRecipeListViewControllerSpec: QuickSpec {
                 }
             }
             
+            context("when it has an active ingredient") {
+                var base: IngredientBase!
+                beforeEach {
+                    base = IngredientBase.all().filter({ $0.name == "Rye" }).first!
+                    controller.activeIngredients = [base]
+                    let _ = controller.view
+                    controller.viewWillAppear(true)
+                }
+
+                it("should have still only have one row for each base") {
+                    
+                    base = IngredientBase.all().filter({ $0.name == "Rye" }).first!
+                    controller.activeIngredients = [base]
+                    let _ = controller.view
+                    controller.viewWillAppear(true)
+                    
+                    let tableView = controller.tableView
+                    var allBases = [IngredientBase]()
+                    Recipe.all().map({
+                        (r: Recipe) in
+                        r.ingredients.map({ $0.base! })
+                    }).filter({
+                        (l: [IngredientBase]) in
+                        l.contains(base)
+                    }).map({
+                        (l: [IngredientBase]) in
+                        l.map({ allBases.append($0) })
+                    })
+                    let uniqueBases = Set(allBases.filter({ $0 != base }).map({ $0.name}))
+                    
+                    let numberOfRows = tableView.numberOfRowsInSection(0)
+                    expect(numberOfRows).to(equal(uniqueBases.count))
+                }
+                
+                it("should not show that active ingredient") {
+                    let tableView = controller.tableView
+                    let rowCount = tableView.numberOfRowsInSection(0) - 1
+                    for row in 0...(rowCount) {
+                        let indexPath = NSIndexPath(forRow: row, inSection: 0)
+                        expect(controller.tableView(tableView, cellForRowAtIndexPath: indexPath).textLabel!.text).toNot(contain("\(base.name)"))
+                    }
+                }
+                
+                
+                it("should prompt to view recipes") {
+                    let tableView = controller.tableView
+                    expect(tableView.tableHeaderView).toNot(beNil())
+                    expect(tableView.tableHeaderView?.subviews.count).to(equal(2))
+                    let headerText = (tableView.tableHeaderView!.subviews.first! as! UIButton).titleLabel?.text
+                    expect(headerText).to(contain("recipes"))
+                    expect(headerText).to(contain("\(base.uses.count)"))
+                }
+                
+            }
+            
         }
 
     }
