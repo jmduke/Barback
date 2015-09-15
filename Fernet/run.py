f55fcf0c3705b3cb2ccdbe40d13849f2217f4283
Justin Duke <dukerson@gmail.com>
2015-09-14 22:24:07 -0400

    Random sleeping for Fernet.

--- Fernet/run.py
+++ Fernet/run.py
@@ -4,6 +4,8 @@ from barbackconfig import *
 import feedparser
 import itertools
 import random
+import time
+
 from slugify import slugify
 import tweepy
 
@@ -15,6 +17,7 @@ TWEETS_TO_FAVORITE = 2
 api = None
 
 favorite_search_terms = ["barback", "scotch", "cocktail"]
+
 
 def check_for_new_posts():
     filename = "barback-rss.txt"
@@ -39,8 +42,10 @@ def check_for_new_posts():
             if slugify(title) not in recent_tweets:
                 return tweet
 
+
 def get_tweet_text():
     return check_for_new_posts()
+
 
 def should_favorite_tweet(tweet, already_favorited_tweets=[], followers_ids=[]):
     return not (any([item in tweet.text for item in TWITTER_BLACKLIST]) or 
@@ -48,6 +53,7 @@ def should_favorite_tweet(tweet, already_favorited_tweets=[], followers_ids=[]):
                 tweet.user.id in api.followers_ids() or
                 any([term in tweet.user.screen_name.lower() for term in favorite_search_terms]) or
                 tweet.user.followers_count / tweet.user.friends_count > 3)
+
 
 def favorite_some_tweets():
     if not TWEETS_TO_FAVORITE:
@@ -72,6 +78,7 @@ def favorite_some_tweets():
             print e
             continue
 
+
 def unfavorite_some_tweets():
     if not TWEETS_TO_FAVORITE:
         return
@@ -81,8 +88,14 @@ def unfavorite_some_tweets():
     for tweet in already_favorited_tweets[:TWEETS_TO_FAVORITE]:
         api.destroy_favorite(tweet)
 
+
 def main():
     global api
+
+    # Do some randomization to make it feel organic.
+    command_interval = 30 * 60
+    delay = random.randint(1, command_interval)
+    time.sleep(delay)
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
