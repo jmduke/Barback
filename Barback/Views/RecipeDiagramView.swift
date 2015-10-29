2f12b4f37143a4a8fd1b71aaa2fa70ad1b0b567f
Justin Duke <dukerson@gmail.com>
2015-10-29 14:29:59 -0700

    Clean up RecipeDiagramViews.

--- Barback/Views/RecipeDiagramView.swift
+++ Barback/Views/RecipeDiagramView.swift
@@ -1,6 +1,13 @@
 import Foundation
 import UIKit
 
+struct RecipeDiagramViewSegment {
+    
+    var viewRatio: Double
+    var color: UIColor
+    
+}
+
 class RecipeDiagramView: UIView {
 
     var recipe: Recipe?  {
@@ -43,6 +50,19 @@ class RecipeDiagramView: UIView {
     func idealWidth() -> CGFloat {
         return glassware.rect().width * CGFloat(diagramScale) + CGFloat(outlineWidth * 4.0 + widthOffset)
     }
+    
+    func drawSegment(strokeWidth: Double, bottomLeft: CGPoint, bottomRight: CGPoint, topLeft: CGPoint, topRight: CGPoint) -> UIBezierPath {
+        let subCanvas = UIBezierPath()
+        subCanvas.lineWidth = CGFloat(strokeWidth)
+        subCanvas.moveToPoint(bottomLeft)
+        subCanvas.addLineToPoint(bottomRight)
+        subCanvas.addLineToPoint(topRight)
+        subCanvas.addLineToPoint(topLeft)
+        subCanvas.addLineToPoint(bottomLeft)
+        subCanvas.closePath()
+        subCanvas.stroke()
+        return subCanvas
+    }
 
     override func drawRect(rect: CGRect) {
         
@@ -52,59 +72,68 @@ class RecipeDiagramView: UIView {
 
         let inset = (topWidth - bottomWidth) / 2
 
-        let totalCount = recipe!.ingredients.map({ Double($0.amount) }).reduce(0.0, combine: +)
+        let totalVolume = recipe!.ingredients.map({ Double($0.amount) }).reduce(0.0, combine: +)
 
-        var ratios: [(Double, UIColor)] = [(0.0, UIColor.redColor())]
+        var viewSegments: [RecipeDiagramViewSegment] = []
+        
+        // Place an initial segment to seed it.
+        viewSegments.append(RecipeDiagramViewSegment(viewRatio: 0.0, color: UIColor.redColor()))
+
+        // It looks nicer with bigger ingredients on the bottom.
         let sortedIngredients = recipe!.ingredients.sort({ $0.amount > $1.amount })
+        
+        // For each ingredient,
+        let whitespaceModifier = (1.0 - EMPTY_SPACE_PROPORTION) / totalVolume
         for ingredient in sortedIngredients {
-            let ratioFraction = (1.0 - EMPTY_SPACE_PROPORTION) * Double(ingredient.amount) / totalCount
+            let ratioFraction =  Double(ingredient.amount) * whitespaceModifier
+            
+            // If the base doesn't actually exist, then there's no color.
             if let base = ingredient.base {
-                ratios.append((ratioFraction + ratios[ratios.count - 1].0, base.uiColor))
+                let previousRatioFraction = viewSegments[viewSegments.count - 1].viewRatio
+                viewSegments.append(RecipeDiagramViewSegment(viewRatio: ratioFraction + previousRatioFraction, color: base.uiColor))
             }
         }
-        ratios.append((1.0, UIColor.whiteColor()))
 
-        for ratioIndex in 0...(ratios.count - 1) {
-            let ratio = ratios[ratioIndex].0
-            let previousRatio = ratioIndex > 0 ? ratios[ratioIndex - 1].0 : ratio
-            
-            let bottomLeft = CGPointMake(CGFloat(widthOffset + inset * (1 - previousRatio)), CGFloat(heightOffset + height * ( 1 - previousRatio)))
-            let bottomRight = CGPointMake(CGFloat(widthOffset + topWidth - (inset * ( 1 - previousRatio))), CGFloat(heightOffset + height * ( 1 - previousRatio)))
-            let topRight = CGPointMake(CGFloat(widthOffset + topWidth - (inset * ( 1 - ratio))), CGFloat(heightOffset + height * ( 1 - ratio)))
-            let topLeft = CGPointMake(CGFloat(widthOffset + inset * (1 - ratio)), CGFloat(heightOffset + height * ( 1 - ratio)))
-            
-            let subCanvas = UIBezierPath()
-            subCanvas.lineWidth = CGFloat(strokeWidth)
-            subCanvas.moveToPoint(bottomLeft)
-            subCanvas.addLineToPoint(bottomRight)
-            subCanvas.addLineToPoint(topRight)
-            subCanvas.addLineToPoint(topLeft)
-            subCanvas.addLineToPoint(bottomLeft)
-            subCanvas.closePath()
+        // Whitespace on the top of the glass.
+        viewSegments.append(RecipeDiagramViewSegment(viewRatio: 1.0, color: UIColor.whiteColor()))
 
-            ratios[ratioIndex].1.setFill()
+        for segmentIndex in 0...(viewSegments.count - 1) {
+            let viewSegment = viewSegments[segmentIndex]
+            let ratio = viewSegment.viewRatio
+            let previousRatio = segmentIndex > 0 ? viewSegments[segmentIndex - 1].viewRatio : ratio
+            
+            let proportionOfSpaceLeftBeforeSegment = 1 - previousRatio
+            let proportionOfSpaceLeftAfterSegment = 1 - ratio
+            
+            let bottomOfSegment = CGFloat(heightOffset + height * proportionOfSpaceLeftBeforeSegment)
+            let topOfSegment = CGFloat(heightOffset + height * proportionOfSpaceLeftAfterSegment)
+            
+            let bottomLeft = CGPointMake(CGFloat(widthOffset + inset * proportionOfSpaceLeftBeforeSegment), bottomOfSegment)
+            let bottomRight = CGPointMake(CGFloat(widthOffset + topWidth - (inset * proportionOfSpaceLeftBeforeSegment)), bottomOfSegment)
+            let topRight = CGPointMake(CGFloat(widthOffset + topWidth - (inset * proportionOfSpaceLeftAfterSegment)), topOfSegment)
+            let topLeft = CGPointMake(CGFloat(widthOffset + inset * proportionOfSpaceLeftAfterSegment), topOfSegment)
+            
+            let subCanvas = drawSegment(strokeWidth, bottomLeft: bottomLeft, bottomRight: bottomRight, topLeft: topLeft, topRight: topRight)
+
+            viewSegment.color.setFill()
             subCanvas.fill()
 
             UIColor.whiteColor().setStroke()
             subCanvas.stroke()
         }
+        
+        let bottomOfGlass = CGFloat(heightOffset + height + strokeWidth)
+        let topOfGlass = CGFloat(heightOffset - strokeWidth)
 
         let bottomExtra = bottomWidth == 0 ? 0 : strokeWidth
-        let bottomLeft = CGPointMake(CGFloat(widthOffset + inset - bottomExtra), CGFloat(heightOffset + height + strokeWidth))
-        let bottomRight = CGPointMake(CGFloat(widthOffset + topWidth - inset + bottomExtra), CGFloat(heightOffset + height + strokeWidth))
-        let topRight = CGPointMake(CGFloat(widthOffset + topWidth + strokeWidth), CGFloat(heightOffset - strokeWidth))
-        let topLeft = CGPointMake(CGFloat(widthOffset - strokeWidth), CGFloat(heightOffset - strokeWidth))
+    
+        let bottomLeft = CGPointMake(CGFloat(widthOffset + inset - bottomExtra), bottomOfGlass)
+        let bottomRight = CGPointMake(CGFloat(widthOffset + topWidth - inset + bottomExtra), bottomOfGlass)
+        let topRight = CGPointMake(CGFloat(widthOffset + topWidth + strokeWidth), topOfGlass)
+        let topLeft = CGPointMake(CGFloat(widthOffset - strokeWidth), topOfGlass)
 
         outlineColor.setStroke()
-        let subCanvas = UIBezierPath()
-        subCanvas.lineWidth = CGFloat(1)
-        subCanvas.moveToPoint(bottomLeft)
-        subCanvas.addLineToPoint(bottomRight)
-        subCanvas.addLineToPoint(topRight)
-        subCanvas.addLineToPoint(topLeft)
-        subCanvas.addLineToPoint(bottomLeft)
-        subCanvas.closePath()
-        subCanvas.stroke()
+        drawSegment(1.0, bottomLeft: bottomLeft, bottomRight: bottomRight, topLeft: topLeft, topRight: topRight)
         
         if diagramScale < 1.0 {
             return
