7b61a4dae2da654bf36d664b4f4c9fba36b45516
Justin Duke <dukerson@gmail.com>
2015-11-24 22:33:14 -0500

    Support for Realm migrations.

--- Rye/Barback/RealmDataSource.swift
+++ Rye/Barback/RealmDataSource.swift
@@ -67,6 +67,24 @@ struct RealmDataSource {
     
     func sync() {
         do {
+            let config = Realm.Configuration(
+                // Set the new schema version. This must be greater than the previously used
+                // version (if you've never set a schema version before, the version is 0).
+                schemaVersion: 1,
+                
+                // Set the block which will be called automatically when opening a Realm with
+                // a schema version lower than the one set above
+                migrationBlock: { migration, oldSchemaVersion in
+                    // We haven’t migrated anything yet, so oldSchemaVersion == 0
+                    if (oldSchemaVersion < 1) {
+                        // Nothing to do!
+                        // Realm will automatically detect new properties and removed properties
+                        // And will update the schema on disk automatically
+                    }
+            })
+            
+            // Tell Realm to use this new configuration object for the default Realm
+            Realm.Configuration.defaultConfiguration = config
             let realm = try Realm()
             try deleteAllData(realm)
             try syncIngredients(realm)
